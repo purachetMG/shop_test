d8907291dde7ee9ff7e568a22244e61e0b0006d7
purachetMG <125813688+purachetMG@users.noreply.github.com>
2023-06-05 13:48:27 +0700

    Add files via upload

---
+++ resources/views/login.blade.php
@@ -0,0 +1,36 @@
+@extends('main')
+
+@section('content')
+
+@if($message = Session::get('success'))
+
+<div class="alert alert-info">
+{{ $message }}
+</div>
+
+@endif
+
+<div class="wrapper fadeInDown">
+    <h2>Login</h2>
+    <div id="formContent">
+  
+        <form action="{{ route('show.validate_login') }}" method="post">
+            @csrf  
+            <input type="text" name="email" class="fadeIn second" placeholder="Email" /><br>
+            @if($errors->has('email'))
+                <span class="text-danger">{{ $errors->first('email') }}</span>
+            @endif
+            <input type="password" name="password" class="fadeIn third" placeholder="Password" /><br>
+            @if($errors->has('password'))
+                <span class="text-danger">{{ $errors->first('password') }}</span>
+            @endif
+        <input type="submit" class="fadeIn fourth" value="Log In">
+    
+      </form>
+  
+    </div>
+  </div>
+
+
+
+@endsection('content')
